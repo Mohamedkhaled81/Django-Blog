83e20a03bb79071b360c41b67cbadef1d71ea7ec
MohamedKhaled81 <mohamed.khaled.200540@gmail.com>
2023-05-05 18:55:49 +0300

    Finish video 5

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,32 +1,13 @@
 from django.shortcuts import render
-from .models import Menu
+from .models import Post
 #from django.http import HttpResponse
 
-posts = [
-    {
-        'author':'Mohamed Khaled',
-        'title':'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'August 27, 2023'
-    },
-    {
-        'author':'Mohamed Zakaria',
-        'title':'Blog Post 2',
-        'content':'second post content',
-        'date_posted':'August 28, 2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title':"about"})
-
-def menu_by_id(request):
-    newmenu = Menu.objects.all()
-    newmenu_dict = {'menu':newmenu}
-    return render(request, 'blog/test.html', newmenu_dict)
