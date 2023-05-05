3da900c86dfeff1f06de632f7b6a6f329b400b89
MohamedKhaled81 <mohamed.khaled.200540@gmail.com>
2023-05-05 23:23:12 +0300

    video 6

---
+++ django_project/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+#from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) # Making form with the data in request.POST
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username') # form.cleaned_data dic
+            messages.success(request,f"Account created for {username}!")
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
